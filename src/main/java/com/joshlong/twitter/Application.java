c2fa96bb55e3287fe9267591d2584f51f9956272
Josh Long <josh@joshlong.com>
2022-08-11 20:39:34 -0700

    polish

--- src/main/java/com/joshlong/twitter/Application.java
+++ src/main/java/com/joshlong/twitter/Application.java
@@ -81,8 +81,7 @@ class SecurityConfiguration {
 			OAuth2AuthorizationRequestResolver authorizationResolver) throws Exception {
 		http//
 				.authorizeHttpRequests(requests -> requests //
-						.mvcMatchers("/actuator/*").permitAll() // todo remove this
-						.anyRequest().authenticated())//
+						.mvcMatchers("/actuator/*").permitAll().anyRequest().authenticated())//
 				.oauth2Login(oauth2 -> oauth2.authorizedClientRepository(oAuth2AuthorizedClientRepository)
 						.authorizationEndpoint(
 								authorization -> authorization.authorizationRequestResolver(authorizationResolver)));
